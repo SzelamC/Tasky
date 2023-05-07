cd87de4d93bf19a5a3c882dc7250aa012e1a3066
sze_lc <chanszelam7725@gmail.com>
2023-05-08 02:20:53 +0800

    remove custom theme test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,6 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
     extend: {
       backgroundImage: {
         "homepage-pattern": "url('/homepage_bg.svg')",
